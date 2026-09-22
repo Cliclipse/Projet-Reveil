23cb14a7c5a1934b18b33eb7a8aae31102b9e826
CorentinRt <corentin.remot@gmail.com>
2026-09-22 15:43:31 -0400

    [ADD] create welcome screen file + comment what to do for next weeks

---
+++ app/src/main/java/com/example/projetreveil/ui/theme/screens/WelcomeScreen.kt
@@ -0,0 +1,19 @@
+package com.example.projetreveil.ui.theme.screens
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun WelcomePage() {
+
+    // Bandeau social top
+
+    // Horloge
+
+    // Afficher heure
+
+    // Liste Reveil
+
+
+    // Bouton + bas droite
+}
