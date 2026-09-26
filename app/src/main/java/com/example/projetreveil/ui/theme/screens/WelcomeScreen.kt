bfc33f4924a0e1594002fe741aca5645664d7d30
CorentinRt <corentin.remot@gmail.com>
2026-09-26 00:50:12 -0400

    [ADD] alarm list (working with lazy column) with wip datas (for testing)

--- app/src/main/java/com/example/projetreveil/ui/theme/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/projetreveil/ui/theme/screens/WelcomeScreen.kt
@@ -4,11 +4,20 @@ import android.icu.util.Calendar
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,7 +30,9 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projetreveil.ui.theme.utilities.DigitalClock
 import kotlinx.coroutines.delay
 import kotlin.math.cos
@@ -29,24 +40,39 @@ import kotlin.math.sin
 import kotlin.time.Duration.Companion.milliseconds
 
 
+data class AlarmData(  // WIP ONLY FOR TESTS in ALARM LIST
+    val hour: Int,
+    val minute: Int,
+    val enabled: Boolean
+)
+
 @Composable
 fun WelcomePage(name: String, modifier: Modifier = Modifier) {
+
+    val exempleAlarms = listOf(
+        AlarmData(7, 25, false),
+        AlarmData(10, 40, true),
+        AlarmData(5, 10, true),
+        AlarmData(15, 59, false),
+        AlarmData(12, 32, true),
+        AlarmData(15, 59, false),
+        AlarmData(15, 59, false),
+        AlarmData(15, 59, false))
 
     Column(modifier = Modifier.fillMaxSize()) {
         // Bandeau social top
 
+        Spacer(modifier = Modifier.height(200.dp))
 
         // Horloge
-        Box(modifier = Modifier.fillMaxWidth().weight(1f), contentAlignment = Alignment.Center)
-        {
+        Box(modifier = Modifier.fillMaxWidth().height(380.dp), contentAlignment = Alignment.Center){
             ClockTimeLive()
         }
 
-        // Afficher heure
-
+        Spacer(modifier = Modifier.height(10.dp))
 
         // Liste Reveil
-
+        AlarmList(exempleAlarms, modifier = Modifier.height(280.dp))
 
         // Bouton + bas droite
     }
@@ -161,3 +187,65 @@ fun ClockTimeLive(){
         )
     }
 }
+
+@Composable
+fun AlarmItem(alarm: AlarmData){
+
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+        Card(modifier = Modifier.width(300.dp).height(60.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.LightGray)
+        ){
+            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp, vertical = 12.dp),
+                verticalAlignment = Alignment.CenterVertically) {
+
+                if (alarm.enabled){
+                    Text(
+                        text = "YES",
+                        fontSize = 28.sp,
+                        color = Color.Green
+                    )
+                }else{
+                    Text(
+                        text = "NO",
+                        fontSize = 28.sp,
+                        color = Color.Gray
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = String.format("%02d:%02d", alarm.hour, alarm.minute),
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun AlarmList(alarms: List<AlarmData>, modifier: Modifier = Modifier){
+
+    Column() {
+        Text(
+            text = "Reveil Perso",
+            modifier = Modifier.fillMaxWidth().height(30.dp).padding(horizontal = 50.dp, vertical = 0.dp),
+            fontSize = 28.sp,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        LazyColumn(modifier = modifier.fillMaxWidth()) {
+            items(alarms) {
+                AlarmItem(it)
+                Spacer(modifier = Modifier.height(6.dp))
+            }
+        }
+    }
+}
+
